35d9d3567d91502884e3fa61d315056a181d5936
ftmvclr <fatimaavcilar@marun.edu.tr>
2026-04-27 19:39:27 +0300

    bit more structured start for the subclasses

--- stringmatchingclasses/MatchingAlgorithms.java
+++ stringmatchingclasses/MatchingAlgorithms.java
@@ -1,9 +1,19 @@
 package stringmatchingclasses;
 
-public class MatchingAlgorithms {
+abstract class MatchingAlgorithms {
+	protected int noOfComparisons;
+	protected int instanceCount;
+	protected int startingIndices[] = new int[128];
+	
 	public static void main(String[] args) {
 		MatchingAlgorithms brute = new BruteForceAlgorithm();
 		MatchingAlgorithms boyer = new BoyerMooresAlgorithm();
 		MatchingAlgorithms horspool = new HorspoolAlgorithm();
+		MatchingAlgorithms array[] = new MatchingAlgorithms[3];
+		for(MatchingAlgorithms a : array ) {
+			a.search();
+		}
 	}
+	
+	public abstract void search(); // this needs to be overridden by the sub classes
 }
