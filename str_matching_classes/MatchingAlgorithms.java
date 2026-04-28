2343b7c50b02c556405105cdfa58bf1e088dbc83
ftmvclr <fatimaavcilar@marun.edu.tr>
2026-04-28 16:14:55 +0300

    highlightHtml method progress
    decided to loop it in reverse to relieve the system of constantly calculating a new offset

--- str_matching_classes/MatchingAlgorithms.java
+++ str_matching_classes/MatchingAlgorithms.java
@@ -8,7 +8,9 @@ import java.util.Scanner;
 abstract class MatchingAlgorithms {
 	protected int noOfComparisons;
 	protected int instanceCount;
-	protected int startingIndices[] = new int[128];
+	protected int startingIndices[] = new int[1024];
+	protected long timeElapsed;
+	protected static int keyLength;
 	
 	public static void main(String[] args){
 		File inputFile = new File("input.html");
@@ -16,26 +18,41 @@ abstract class MatchingAlgorithms {
 		MatchingAlgorithms brute = new BruteForceAlgorithm();
 		MatchingAlgorithms boyer = new BoyerMooresAlgorithm();
 		MatchingAlgorithms horspool = new HorspoolAlgorithm();
-		MatchingAlgorithms array[] = {brute, boyer, horspool};
+		MatchingAlgorithms algosArray[] = {brute, boyer, horspool};
 		
-		for(MatchingAlgorithms a : array){
+		for(MatchingAlgorithms a : algosArray){
 			try (Scanner inputScanner = new Scanner(inputFile)){
 	            a.search(inputScanner);
-	            highlightHtml(a.startingIndices, inputFile);   
+	            highlightHtml(a, inputFile);   
 	        } 
 			catch (FileNotFoundException e){
-	            System.err.println("Error: Could not find the file!");
 	            e.printStackTrace();
 	            System.exit(1);
 	        }
 		}
 	}
-	private static void highlightHtml(int indices[], File inputFile){
+	// needs key length and a for loop that goes from 0 to instance count
+	// loop this backward for indices' not to move
+	private static void highlightHtml(MatchingAlgorithms algo, File inputFile){
 		try(PrintWriter pw = new PrintWriter(inputFile);) {
-			// needs key length and a for loop that goes from 0 to instance count
+			int[] startingIndices = algo.startingIndices;
+			String markStart = "<mark>";
+			String markEnd = "</mark>";
+			StringBuilder sb = new StringBuilder();
+			Scanner scanner = new Scanner(inputFile);
+			while (scanner.hasNextLine()) {
+		        sb.append(scanner.nextLine());
+		        sb.append("\n"); // TODO check if \r\n is a requirement 
+		    }
+			for(int i = algo.instanceCount - 1; i >= 0; i--) {
+				sb.insert(startingIndices[i] + keyLength, markEnd);
+				sb.insert(startingIndices[i], markStart);
+			}
+			scanner.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+		
 	}
 	
 	public abstract void search(Scanner scanner); // this needs to be overridden by the sub classes
