13ccd481159b1cbcfe268f897c7bd7a767c3d3a8
ftmvclr <fatimaavcilar@marun.edu.tr>
2026-04-29 22:39:05 +0300

    better structured io
    + typos

--- str_matching_classes/MatchingAlgorithms.java
+++ str_matching_classes/MatchingAlgorithms.java
@@ -9,38 +9,42 @@ abstract class MatchingAlgorithms {
 	PrintWriter pw;
 	protected int noOfComparisons;
 	protected int instanceCount;
-	protected int startingIndices[] = new int[1024];
+	protected int startingIndices[] = new int[1024]; // should we use array lists instead?
 	protected long timeElapsed;
 	protected static int keyLength;
- 	protected String keyPattern;
+ 	protected static String keyPattern;
+ 	protected static long textLength;
 	
 	public static void main(String[] args) throws FileNotFoundException{
 		File inputFile = new File("input.html");
+		File patternFile = new File("key.txt");
 		MatchingAlgorithms brute = new BruteForceAlgorithm(new PrintWriter("bruteOutput.html"));
-		MatchingAlgorithms boyer = new BoyerMooresAlgorithm(new PrintWriter("bruteOutput.html"));
-		MatchingAlgorithms horspool = new HorspoolAlgorithm(new PrintWriter("bruteOutput.html"));
+		MatchingAlgorithms boyer = new BoyerMooresAlgorithm(new PrintWriter("boyerOutput.html"));
+		MatchingAlgorithms horspool = new HorspoolAlgorithm(new PrintWriter("horspoolOutput.html"));
 		MatchingAlgorithms algosArray[] = {brute, boyer, horspool};
 		
 		Scanner inputScanner = new Scanner(inputFile);
-		StringBuilder sb = new StringBuilder();
-		while (inputScanner.hasNextLine()) {
-			sb.append(inputScanner.nextLine());
-			sb.append("\n"); // TODO check if \r\n is a requirement 
-			for(MatchingAlgorithms a : algosArray){
-	            a.search(sb.toString());
-	            highlightHtml(a, inputFile, sb); 
-	            inputScanner.close();
-			}
+		Scanner patternScanner = new Scanner(patternFile);
+		// text
+		StringBuilder text = stringBuilderBuilder(inputScanner);
+		textLength = text.toString().length();
+        // key
+        StringBuilder key = stringBuilderBuilder(patternScanner);
+        keyPattern = key.toString();
+        keyLength = key.length();
+        // search
+		for(MatchingAlgorithms a : algosArray){
+            a.search(text.toString());
+            highlightHtml(a, inputFile, text); 
 		}
 	}
-	// needs key length and a for loop that goes from 0 to instance count
-	// loop this backward for indices' not to move
+	
 	private static void highlightHtml(MatchingAlgorithms algo, File inputFile, StringBuilder sb){
 		try(PrintWriter pw = new PrintWriter(inputFile);) {
 			int[] startingIndices = algo.startingIndices;
 			String markStart = "<mark>";
 			String markEnd = "</mark>";
-			for(int i = algo.instanceCount - 1; i >= 0; i--) { // as long as instanceCount isnt > 1024
+			for(int i = algo.instanceCount - 1; i >= 0; i--) { // as long as instanceCount isn't > 1024
 				sb.insert(startingIndices[i] + keyLength, markEnd);
 				sb.insert(startingIndices[i], markStart);
 			}
@@ -54,7 +58,15 @@ abstract class MatchingAlgorithms {
 		this.pw.write(sb.toString());
 	}
 	
-	abstract protected void search(String text); // this needs to be overridden by the sub classes
-	// while searching it will adjust its own fields appropriately
-	// could also fill the startingIndices array
+	private static StringBuilder stringBuilderBuilder(Scanner inputScanner) {
+		StringBuilder sb = new StringBuilder();
+		while (inputScanner.hasNextLine()) {
+			sb.append(inputScanner.nextLine());
+			sb.append("\n"); // TODO check if \r\n is a requirement 
+		}
+		inputScanner.close();
+		return sb;
+	}
+	/*filling in the required fields while searching for instances*/
+	abstract protected void search(String text);
 }
