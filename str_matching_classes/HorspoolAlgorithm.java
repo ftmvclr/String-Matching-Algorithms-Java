df4c37bb1d9bf0bd42344a02fc892f06b4bb4d1c
Senae00 <senaeabdu2@gmail.com>
2026-04-30 04:51:09 +0300

    horspool added

--- str_matching_classes/HorspoolAlgorithm.java
+++ str_matching_classes/HorspoolAlgorithm.java
@@ -1,18 +1,51 @@
 package str_matching_classes;
 
 import java.io.PrintWriter;
-import java.util.Scanner;
 
 public class HorspoolAlgorithm extends MatchingAlgorithms {
-	PrintWriter pw;
-	
-	public HorspoolAlgorithm(PrintWriter pw) {
-		pw = this.pw;
-	}
-	
-	@Override
-	public void search(Scanner scanner) {
-		
-	}
 
+    public HorspoolAlgorithm(PrintWriter pw) {
+        this.pw = pw;
+    }
+
+    private int[] badTable(String pattern) {
+        int[] table = new int[256];
+        int p=pattern.length();
+        for(int i = 0; i < 256; i++) {
+            table[i] = p;
+        }
+        for(int i=0; i<p-1; i++) {
+            table[pattern.charAt(i)] = p-1-i;
+        }
+        return table;
+    }
+
+    @Override
+    protected void search(String text) {
+        String pattern=keyPattern;
+        int m=pattern.length();
+        int n=text.length();
+        int[] table=badTable(pattern);
+        int i=m-1;
+        long start=System.nanoTime(); 
+        while(i<n){
+            int k=0;
+            while(k<m){
+                char patt= pattern.charAt(m-1-k);
+                char tex= text.charAt(i-k);
+                noOfComparisons++;
+                if(patt!=tex){
+                    break;
+                }
+                k++; 
+            }
+                if (k==m){
+                    startingIndices[instanceCount]=i-m+1;
+                    instanceCount++;
+                }
+            i+=table[text.charAt(i)];
+        }
+        long end=System.nanoTime();
+        timeElapsed=end-start;
+    }
 }
